8c6606e97b41e4542d0538c1e299b386dea51f9e
PiaPsyker <PiaPsyker@WIN10-PSYKER>
2023-02-21 20:50:57 +0100

    Added "Record Statement" func

--- Magnus-Archival-System.cpp
+++ Magnus-Archival-System.cpp
@@ -6,17 +6,17 @@
 
 void credits() {
     std::cout << "\n" << std::endl;
-    std::cout << "\t\t\t\t\tษอออออออออออออออออออออออออออป" << std::endl;
-    std::cout << "\t\t\t\t\tบ Lead Developer: PiaPsyker บ" << std::endl;
-    std::cout << "\t\t\t\t\tบ Art: PiaPsyker            บ" << std::endl;
-    std::cout << "\t\t\t\t\tบ Art: Fliedi               บ" << std::endl;
-    std::cout << "\t\t\t\t\tบ Inspiration: Fliedi       บ" << std::endl;
-    std::cout << "\t\t\t\t\tบ Mental Support: Fliedi    บ" << std::endl;
-    std::cout << "\t\t\t\t\tบ <3                        บ" << std::endl;
-    std::cout << "\t\t\t\t\tศอออออออออออออออออออออออออออผ" << std::endl;
+    std::cout << "\t\t\t\t\tษอออออออออออออออออออออออออออออออป" << std::endl;
+    std::cout << "\t\t\t\t\tบ Lead Developer: PiaPsyker     บ" << std::endl;
+    std::cout << "\t\t\t\t\tบ Assisting Developer: BlckBrst บ" << std::endl;
+    std::cout << "\t\t\t\t\tบ Art: PiaPsyker                บ" << std::endl;
+    std::cout << "\t\t\t\t\tบ Art: Fliedi                   บ" << std::endl;
+    std::cout << "\t\t\t\t\tบ Inspiration: Fliedi           บ" << std::endl;
+    std::cout << "\t\t\t\t\tบ Mental Support: Fliedi        บ" << std::endl;
+    std::cout << "\t\t\t\t\tบ <3                            บ" << std::endl;
+    std::cout << "\t\t\t\t\tศอออออออออออออออออออออออออออออออผ" << std::endl;
     std::cout << "\n";
 }
-
 
 void initial() {
     std::cout << "\n" << std::endl;
@@ -30,6 +30,49 @@ void initial() {
     std::cout << "\n";
 }
 
+void record_statement() {
+    // Author
+    // Statement Giver
+    // Type of Statement
+    // Subject / Short Description
+    std::ofstream statement;
+    // TODO Set name to in-game date
+    statement.open("statement.txt", std::ios::out | std::ios::trunc);
+
+    std::string author;
+    std::string statement_giver;
+    std::string type;
+    std::string subject;
+    std::string statement_txt;
+
+    std::cout << "\n>> NEW STATEMENT <<" << std::endl;
+    std::cout << "\nAuthor: ";
+    std::getline(std::cin, author);
+    statement << "Author: " + author + "\n";
+
+    std::cout << "\nStatement giver: ";
+    std::getline(std::cin, statement_giver);
+    statement << "Statement giver: " + statement_giver + "\n";
+
+    std::cout << "\nType of Statement: ";
+    std::getline(std::cin, type);
+    statement << "Type of Statement: " + type + "\n";
+
+    std::cout << "\nSubject: ";
+    std::getline(std::cin, subject);
+    statement << "Subject: " + subject + "\n";
+
+    std::cout << "\Statement: ";
+    std::getline(std::cin, statement_txt);
+    statement << "Statement: " + statement_txt + "\n";
+
+    statement.close();
+}
+
+void read_statement();
+
+void research();
+
 void show_options() {
     std::cout << "\t\t\t\t\tอ> [1] Record new statement" << std::endl;
     std::cout << "\t\t\t\t\tอ> [2] Read statement" << std::endl;
@@ -42,6 +85,7 @@ void show_options() {
     }
     if (io == "1") {
         std::cout << "\t\t\t\t\t>>>> RECORD NEW STATEMENT <<<<" << std::endl;
+        record_statement();
     } else if (io == "2") {
         std::cout << "\t\t\t\t\t>>>> READ STATEMENT <<<<" << std::endl;
     } else {
@@ -76,11 +120,8 @@ int title_screen() {
     }
 }
 
-void record_statement();
-
-void read_statement();
-
-void research();
+// TODO Time System #yyymmdd -A -B :: #8141206 June 12th 1841
+//                            ^  ^ If multiple statements in one day
 
 int main()
 {
@@ -108,6 +149,7 @@ int main()
         std::cout << "EXITING..." << std::endl;
         exit(0);
     }
+
     // New game
     while (game_state == 1) {
         
@@ -115,6 +157,7 @@ int main()
         std::cout << "\n>>>> What is your name?\nอ> ";
         std::getline(std::cin, pname);
         player.set_name(pname);
+
         // Opens save file for new player
         sfile.open(pname +".bin", std::ios::out | std::ios::trunc | std::ios::binary);
         sfile << pname << "\n";
@@ -142,6 +185,14 @@ int main()
         sfile << ppronouns << "\n";
 
         // TODO Job
+        std::string job;
+        while (job != "0" && job != "1" && job != "2" && job != "3" && job != "4" && job != "5" && job != "6" && job != "7" && job != "8" && job != "98" && job != "99") {
+            std::cout << "\n>>>> What is your Job at the Magnus Institute?\n\n[0] Archivist\n[1] Archival Assistant\n[2] Artifact Storage\n[3] Librarian\n[4] Researcher\n[5] Archive Security\n[6] Receptionist\n[7] Administrator\n[8] Janitor\n\nEnter Number of Jobอ> ";
+            std::getline(std::cin, job);
+        }
+        pjob = std::stoi(job);
+        player.set_job(pjob);
+        sfile << pjob << "\n";
         
         // Sets game state for save file to 5
         sfile << "5" << "\n";
@@ -152,6 +203,7 @@ int main()
         std::cout << "\n\t\t\t\t\t>>>> NEW USER '" << pname << "' CREATED" << std::endl;
         game_state = 5;
     }
+
     // Load game
     while (game_state == 2) {
         // TODO
